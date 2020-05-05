d9e1d6c4ed6078c74783cbe1da25feec06b6324d
jurmann <joseph@swoopsrch.com>
2020-05-05 00:32:30 -0600

    added components folder with one component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { CardList } from "./components/card-list/card-list.jsx";
+
+import "./App.css";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            string: 'Hello Jose'
-        }
-    }
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>{this.state.string}</p>
-                    <button onClick={() => this.setState({ string: 'Hello Carlos'})}>Change name</button>
-                </header>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  async componentWillMount() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users/");
+    response = await response.json();
+    this.setState({ monsters: response });
+    console.log(response);
+  }
+  render() {
+    return (
+      <div className="App">
+        <CardList>
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}> {monster.name}</h1>
+          ))}
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
