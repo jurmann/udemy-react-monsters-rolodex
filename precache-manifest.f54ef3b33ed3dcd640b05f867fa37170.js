ae9369c61d31236aeef30e9ffb4692f28676986b
Joseph Urmann <joseph.urmann@pwc.com>
2020-05-08 00:08:36 -0600

    Updates

---
+++ precache-manifest.f54ef3b33ed3dcd640b05f867fa37170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480b818e85137b72ee8b1f74ab423784",
+    "url": "/udemy-react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b066ba0b76e14eee1e92",
+    "url": "/udemy-react-monsters-rolodex/static/css/main.bd5b10ef.chunk.css"
+  },
+  {
+    "revision": "b7025d703d6faac98ebe",
+    "url": "/udemy-react-monsters-rolodex/static/js/2.a0f84a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy-react-monsters-rolodex/static/js/2.a0f84a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b066ba0b76e14eee1e92",
+    "url": "/udemy-react-monsters-rolodex/static/js/main.3cda81c1.chunk.js"
+  },
+  {
+    "revision": "6a6c23bc29624548f167",
+    "url": "/udemy-react-monsters-rolodex/static/js/runtime-main.bd07de23.js"
+  }
+]);
